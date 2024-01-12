d9a8490b27191ea0c9a8847dc0c6be1748508018
whdgus9269 <whdgus9269@munch-factory.com>
2024-01-12 15:38:31 +0900

    javascript 엔진 중간 커밋

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,15 +13,16 @@ export default function RootLayout({children}) {
     return (
         <html lang="en">
             <body className="flex flex-col items-center relative">
-                <main className={`
+                <main className={`relative
                 w-full bg-gray030 min-h-screen h-full
                 desktop:max-w-[2200px] 
                 desktop:px-[48px] desktop:py-[48px]
                 tablet:px-[32px] tablet:py-[32px] 
                 mobile:px-[20px] mobile:py-[20px] 
                 `}>
+
                     <section className={`
-                    w-full h-full flex mobile:flex-col gap-[20px]  
+                    w-full h-full flex mobile:flex-col gap-[20px] z-[1] relative
                     `}>
                         <NavigationHeader />
 
@@ -30,6 +31,11 @@ export default function RootLayout({children}) {
                             {children}
                         </div>
                     </section>
+
+
+                    <div className="w-full absolute top-0 left-0 h-[266px] blur-[8px]">
+                        <img src="/banner.jpg" className="w-full h-full"/>
+                    </div>
                 </main>
             </body>
 
--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -5,9 +5,35 @@ export default function NavigationHeader() {
 
     return (
         <>
-            <header className="hidden mobile:block bg-white">
+            <header className="hidden mobile:block bg-white/70 rounded-[8px] rounded-[8px]">
+
+
+
+                <div className="flex items-center justify-center mt-[12px] gap-[8px]">
+                    <img src="/profile.jpeg" className="w-[40px] h-[40px] rounded-full object-cover"/>
+                    <p className="heading-sm text-gray080">유종현 /</p>
+                    <p className="heading-xs text-gray080">whdgus9269@gmail.com /</p>
+                    <p className="heading-xs text-gray080">01083889128 /</p>
+                </div>
+
+
+                <div className="flex flex-col items-center justify-center mt-[20px] flex-1">
+                    <div className="flex flex-col items-center justify-center">
+                        <p className="text-gray080 heading-md">언어</p>
+                        <p className="text-gray080 body-xs text-center mt-[4px]">JavaScript, TypeScript<br/>마크업 언어 - HTML, CSS</p>
+                    </div>
+                    <div className="flex flex-col items-center justify-center flex-1 mt-[12px] mb-[20px]">
+                        <p className="text-gray080 heading-md">라이브러리 및 프레임워크</p>
+                        <p className="text-gray080 body-xs text-center mt-[4px]">React, Next.js</p>
+                        <p className="text-gray080 body-xs text-center">tailwindcss, react-query, recoil, styled-component</p>
+                    </div>
+                </div>
+
+
+
+
             </header>
-            <header className="mobile:hidden block pt-[25px] pb-[16px] px-[30px] bg-white w-[300px] min-w-[300px] h-full">
+            <header className="mobile:hidden block py-[40px] rounded-[8px] px-[30px] bg-white/70 w-[300px] min-w-[300px] h-full">
                 <div className="flex flex-col items-center justify-center">
                     <img src="/profile.jpeg" className="w-[110px] h-[110px] rounded-full object-cover"/>
                     <p className="mt-[20px] heading-md text-gray080">유종현</p>
