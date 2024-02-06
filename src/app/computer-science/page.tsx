97271e7193a2835cefb428c22be225392c8e9ef0
whdgus9269 <whdgus9269@munch-factory.com>
2024-02-06 14:17:39 +0900

    브라우저 렌더링 추가, tcp 추가중

--- src/app/computer-science/page.tsx
+++ src/app/computer-science/page.tsx
@@ -1,27 +1,28 @@
 "use client"
 import {useRouter} from "next/navigation";
-const parentStyle = "flex flex-col items-start p-[20px] pb-[40px] bg-white/70 mt-[20px] rounded-[12px]"
+const parentStyle = "flex flex-col items-start p-[20px] pb-[40px] bg-white/70 mt-[20px] rounded-[12px] gap-[8px]"
 
 
 const csData = [
-    {title_ : "http 버전 특징",  link : "/computer-science/http-version" }
-
+    {title_ : "http 버전 특징",  link : "/computer-science/http-version" },
+    {title_ : "osi 7계층",  link : "/computer-science/osi" },
+    {title_ : "tcp",  link : "/computer-science/tcp" },
 ]
 export default function Cs() {
 
     const router = useRouter()
 
     return (
-        <article className={parentStyle}>
+        <section className={parentStyle}>
             {csData.map((value, index) => (
                 <div
                     key={index}
                     onClick={() => { router.push(value.link) }}
-                    className="p-[8px] flex flex-col gap-[4px] bg-gray040 rounded-[8px]">
+                    className="p-[8px] flex flex-col gap-[4px] bg-gray040 rounded-[8px] cursor-pointer">
                     <p className="body-md text-gray090">{value.title_}</p>
                 </div>
             ))}
-        </article>
+        </section>
     )
 
 }
